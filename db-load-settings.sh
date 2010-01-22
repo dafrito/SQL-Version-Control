ae552972aa178f3130347e67c4f77eb9edf67cbe
Aaron Faanes <dafrito@gmail.com>
2010-01-22 08:16:23 -0600

    Fixed priority issues in load-settings

--- db-load-settings.sh
+++ db-load-settings.sh
@@ -4,12 +4,43 @@ PATH=/bin:/usr/bin:$HOME/bin:${0%/*}
 
 FORCE=1
 
+function load_settings {
+    if [ ! -e $1 ]; then
+        echo "$0: $1 does not exist" 1>&2; exit 1
+    fi;
+    if ! . $1; then
+        exit 1
+    fi
+    SETTINGS=$1; 
+}
+
+args=$*
 IFS='   
 ='
 while [ -n "$1" ]; do
     case "$1" in
-        -d|-db|--database) DATABASE=$2; shift ;;
         -s|--settings) SETTINGS=$2; shift ;;
+        -*) ;;
+        *) 
+            if [ -e $1 ]; then
+                SETTINGS=$1
+            fi
+    esac
+    shift
+done
+if [ -e ./db-settings ]; then
+    load_settings ./db-settings
+fi;
+if [ -n "$DATABASE" ] && [ -e $DATABASE.db-settings ]; then
+    load_settings ./$DATABASE.db-settings
+fi
+set $args
+while [ -n "$1" ]; do
+    case "$1" in
+        -d|-db|--database) DATABASE=$2; shift ;;
+        -s|--settings) 
+            load_settings $2
+            shift ;;
         -p|-pw|--password) PASSWORD=$2; shift ;;
         -f|--force) FORCE=0 ;;
         -i|--interactive) FORCE=1 ;;
@@ -17,39 +48,22 @@ while [ -n "$1" ]; do
         -*) echo "$0: Unknown argument $1" 1>&2; exit 1 ;;
         *) 
             if [ -e $1 ]; then
-                SETTINGS=$1
+                load_settings $1
             else
                 DATABASE=$1;
             fi
     esac
     shift
 done
-
-explicit_settings=$SETTINGS
-if [ -e ./db-settings ]; then
-    . ./db-settings
-    SETTINGS=./db-settings
-fi;
-if [ -n "$DATABASE" ] && [ -e $DATABASE.db-settings ]; then
-    . ./$DATABASE.db-settings
-    SETTINGS=./$DATABASE.db-settings
-fi
-if [ -n "$explicit_settings" ]; then
-    if [ -e "$explicit_settings" ]; then
-        echo $explicit_settings
-        . $explicit_settings
-        SETTINGS=$explicit_seetings
-    else
-        echo "$0: Settings not found at $explicit_settings"
-        exit 1
-    fi
-fi
-unset explicit_settings
 PATCH_ROOT=./patches
 
 db=$DATABASE
 root=$PATCH_ROOT
-patchlist=patchlist
+if [ -e $db.patchlist ]; then
+    patchlist=$db.patchlist;
+else
+    patchlist=patchlist
+fi
 
 if [ "${db_utils_DONT_VERIFY}" != 0 ]; then
     echo ${SETTINGS?"db-settings not found. Set with -s, --settings, or run $0 from a dir that contains db-settings"} >/dev/null
