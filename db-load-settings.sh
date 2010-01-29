794f7cdffb74131ff8d8fa56d3926d5ec82f37be
Aaron Faanes <dafrito@gmail.com>
2010-01-29 03:24:58 -0600

    Major load-settings changes
    * Global settings no longer recognized
    * database must always be specified, unless default is used
    * settings now live in .db, rather than in the directory
    * $db.password now houses the pw, for easy gitignore convenience

--- db-load-settings.sh
+++ db-load-settings.sh
@@ -1,79 +1,75 @@
 #!/bin/sh
-# Updates a database by running patches
+# Loads settings for the specified db
 PATH=/bin:/usr/bin:$HOME/bin:${0%/*}
 
 DUMP_TABLES=0
 
-function load_settings {
-    if [ ! -e $1 ]; then
-        echo "$0: $1 does not exist" 1>&2; exit 1
-    fi;
-    if ! . $1; then
-        exit 1
-    fi
-    SETTINGS=$1; 
-}
+CONFIG_ROOT='./.db'
 
 args=$*
 IFS='   
 ='
 while [ -n "$1" ]; do
     case "$1" in
-        -s|--settings) SETTINGS=$2; shift ;;
+        -d|-db|--database) db=$2; shift ;;
         -*) ;;
         *) 
-            if [ -e $1 ]; then
-                SETTINGS=$1
-            fi
+            db=$1
     esac
     shift
 done
-if [ -e ./db-settings ]; then
-    load_settings ./db-settings
-fi;
-if [ -n "$DATABASE" ] && [ -e $DATABASE.db-settings ]; then
-    load_settings ./$DATABASE.db-settings
+
+if [ -z $db ]; then
+    if [ -e $CONFIG_ROOT/default ]; then
+        db=$(cat $CONFIG_ROOT/default);
+    fi
 fi
+if [ -n $db ]; then
+    SETTINGS=$CONFIG_ROOT/$db.settings;
+    if [ -e $SETTINGS ]; then
+        if ! . $SETTINGS; then
+            ERROR=0;
+        fi
+    fi
+    if [ -e $CONFIG_ROOT/$db.password ]; then
+        PASSWORD=$(cat $CONFIG_ROOT/$db.password);
+    fi
+fi
+
 set -- $args >/dev/null
 while [ -n "$1" ]; do
     case "$1" in
-        -d|-db|--database) DATABASE=$2; shift ;;
-        -s|--settings) 
-            load_settings $2
-            shift ;;
+        -d|-db|--database) shift ;;
         -p|-pw|--password) PASSWORD=$2; shift ;;
         -f|--force) FORCE=0 ;;
-        -i|--interactive) FORCE=1 ;;
-        -l|--log) LOG=$2; shift ;;
-        --no-dump) unset DUMP_TABLES ;;
+        -i|--interactive) unset FORCE ;;
         --dump) DUMP_TABLES=0 ;;
-        -*) echo "$0: Unknown argument $1" 1>&2; exit 1 ;;
-        *) 
-            if [ -e $1 ]; then
-                load_settings $1
-            else
-                DATABASE=$1;
-            fi
+        --no-dump) unset DUMP_TABLES ;;
     esac
     shift
 done
 set -- $args >/dev/null
-PATCH_ROOT=./patches
 
-db=$DATABASE
-root=$PATCH_ROOT
+root=./patches
 if [ -e $db.patchlist ]; then
     patchlist=$db.patchlist;
 else
     patchlist=patchlist
 fi
 
-if [ "${db_utils_DONT_VERIFY}" != 0 ]; then
-    echo ${SETTINGS?"db-settings not found. Set with -s, --settings, or run $0 from a dir that contains db-settings"} >/dev/null
-    echo ${DATABASE?"$0: DATABASE not set. Set with -d, -db, --database, or specify in db-settings"} >/dev/null
-    echo ${LOG='./db-$db.log'} >/dev/null
+if [ ! $DB_DONT_COMPLAIN ]; then
+    if [ $ERROR ]; then
+        echo "db: settings encountered an error: $SETTINGS"
+    fi
+    if [ -z $db ]; then
+        echo "db: no database specified and there is no default" 1>&2;
+    fi
+    if [ ! -e $CONFIG_ROOT/$db.settings ]; then
+        echo "db: $CONFIG_ROOT/$db.settings does not exist" 1>&2; 
+        exit 1
+    fi
     if [ ! -e $patchlist ]; then
-        echo "$0: $patchlist not found" 1>&2
+        echo "db: $patchlist not found" 1>&2
         exit 1
     fi
 fi
