9f4aa8995ff2215023d880780bf73ffa385723aa
Aaron Faanes <dafrito@gmail.com>
2010-01-29 05:32:12 -0600

    load-settings now uses a neater error function

--- db-load-settings.sh
+++ db-load-settings.sh
@@ -6,6 +6,14 @@ DUMP_TABLES=0
 
 CONFIG_ROOT='./.db'
 
+function error {
+    if [ $DB_DONT_COMPLAIN ]; then
+        return;
+    fi
+    echo $* 1>&2
+    exit 1
+}
+
 args=$*
 IFS='   
 ='
@@ -24,19 +32,34 @@ if [ -z $db ]; then
         db=$(cat $CONFIG_ROOT/default);
     fi
 fi
-if [ -e $CONFIG_ROOT/settings ]; then
+
+if [ -z $db ]; then
+    IFS='
+'
+    set -- $(ls -1 *.list) >/dev/null
+    case "$#" in
+        1) db=${1%.*} ;;
+        0) error "db: no database or patchlist found" ;;
+        *) error "db: multiple lists found: " $* ;;
+    esac
+    set -- $args >/dev/null
+fi
+
+if [ -s $CONFIG_ROOT/settings ]; then
     SETTINGS=$CONFIG_ROOT/settings;
     if ! . $SETTINGS; then
         ERROR=$SETTINGS;
     fi
 fi
-if [ -e $CONFIG_ROOT/$db/settings ]; then
+if [ -s $CONFIG_ROOT/$db/settings ]; then
     SETTINGS=$CONFIG_ROOT/$db/settings;
     if ! . $SETTINGS; then
         ERROR=$SETTINGS;
     fi
 fi
 
+IFS='   
+='
 set -- $args >/dev/null
 while [ -n "$1" ]; do
     case "$1" in
@@ -47,25 +70,27 @@ while [ -n "$1" ]; do
         --dump) DUMP_TABLES=0 ;;
         --no-dump) unset DUMP_TABLES ;;
         -r|--root) root=$2; shift ;;
+        -l|--log) log=$2; shift ;;
     esac
     shift
 done
 set -- $args >/dev/null
 
+if [ -z $log ]; then
+    if [ -e $CONFIG_ROOT ]; then
+        if [ -e $CONFIG_ROOT/$db ]; then
+            log=$CONFIG_ROOT/$db/log
+        else
+            log=$CONFIG_ROOT/log
+        fi
+    fi
+fi
 root=${root-'.'}
 patchlist=$db.list;
 
-if [ ! $DB_DONT_COMPLAIN ]; then
-    if [ $ERROR ]; then
-        echo "db: error while loading '$ERROR'"
-        exit 1
-    fi
-    if [ -z $db ]; then
-        echo "db: no database specified and there is no default" 1>&2;
-        exit 1
-    fi
-    if [ ! -e $patchlist ]; then
-        echo "db: $patchlist not found" 1>&2
-        exit 1
-    fi
+if [ -z $db ]; then
+    error "db: no database specified and there is no default";
+fi
+if [ ! -e $patchlist ]; then
+    error "db: $patchlist not found"
 fi
