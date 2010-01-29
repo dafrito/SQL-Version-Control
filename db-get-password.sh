5798fe83a42e343bd4bd2d396bab0c121f1ec245
Aaron Faanes <dafrito@gmail.com>
2010-01-29 03:29:36 -0600

    get-password doesn't load its own settings, now uses $db

--- db-get-password.sh
+++ db-get-password.sh
@@ -2,14 +2,12 @@
 # Retrieves a password and verifies a log-in to the specified db.
 PATH=/bin:/usr/bin:$HOME/bin:${0%/*}
 
-. db-load-settings.sh >/dev/null
-
 if [ -z "$PASSWORD" ]; then
     trap 'stty echo; echo; exit 1' INT TERM
     read -s -p'MySQL Password: ' PASSWORD
     echo
 fi
 
-if ! mysql $DATABASE -p$PASSWORD -e"use $DATABASE"; then
+if ! mysql $db -p$PASSWORD -e"use $db"; then
     exit 2
 fi
