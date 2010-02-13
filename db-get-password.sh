af11b535e42d105ec3703600ed9ddb2325c656fd
Aaron Faanes <dafrito@gmail.com>
2010-02-13 17:39:25 -0600

    get-password now uses db-library

--- db-get-password.sh
+++ db-get-password.sh
@@ -2,12 +2,14 @@
 # Retrieves a password and verifies a log-in to the specified db.
 PATH=/bin:/usr/bin:$HOME/bin:${0%/*}
 
+source db-library.sh
+
 if [ -z "$PASSWORD" ]; then
     trap 'stty echo; echo; exit 1' INT TERM
     read -s -p"Password for '$db': " PASSWORD
     echo
 fi
 
-if ! mysql $db -u "$user" -p$PASSWORD -e"use $db"; then
+if ! sql "use $db"; then
     exit 2
 fi
